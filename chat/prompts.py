fe08a056bf4660409d14c459f18f5ffcc14f3bf8
ruben.correia <ruben.correia@uni-konstanz.de>
2025-12-10 21:19:34 +0100

    Promt improved

--- chat/prompts.py
+++ chat/prompts.py
@@ -1,3 +1,3 @@
 SYSTEM_PROMPT = """
-You are a personal trainer. Upon request, create a training plan 
+You are a personal trainer. Upon request, create a stretching routine that takes maximum 10min. Only give 5 exercises per routine and only give the name and the length the person should hold the strech for. 
 """
